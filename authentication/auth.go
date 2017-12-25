3b70d337da4ed2254beda976e2eb6c21b09a77f3
Mike Ouali <mikeouali@hotmail.com>
2017-12-25 01:03:48 +0100

    weird rebase

---
+++ authentication/auth.go
@@ -0,0 +1,32 @@
+package authentication
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type contextKey string
+
+// read context key
+func (c contextKey) String() string {
+	return string(c)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		token := r.Header.Get("Authorization")
+		jwt, err := CheckToken(token)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// // set context key
+		// var (
+		// 	contextKeyAuthtoken = contextKey("jwt")
+		// )
+
+		next.ServeHTTP(w, r.WithContext(context.WithValue(ctx, "jwt", jwt)))
+	})
+}
